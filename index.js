3fca3e84315040914dd0e525a59d3570fa17b93a
Langford Kwabena <langfordquarshie21@gmail.com>
2023-01-30 13:27:28 +0000

    Update index.js - Change URL for generateCommitMessage endpoint

--- index.js
+++ index.js
@@ -81,7 +81,8 @@ const stageChanges = async () => {
 const generateCommitMessage = async () => {
     let config = {
         method: 'post',
-        url: "http://localhost:3212/generate-commit-message",
+        // url: "http://localhost:3212/generate-commit-message",
+        url: "https://ai-server-qjof.onrender.com/generate-commit-message",
         headers: { "Content-Type": "application/json" },
         data: JSON.stringify({ "code": newGitChanges })
     }
